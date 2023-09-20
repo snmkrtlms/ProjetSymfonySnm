9881d7253de8a64ae62ee4168de1f15493b08ddb
Senem Kurtulmus <kurtulmus.senem@hotmail.com>
2023-09-20 12:05:51 +0200

    fixture load

--- bdDeleteCreateMigrateFill.bat
+++ bdDeleteCreateMigrateFill.bat
@@ -3,4 +3,4 @@ symfony console doctrine:database:drop --force
 symfony console doctrine:database:create 
 symfony console make:migration 
 symfony console doctrine:migrations:migrate 
-symfony console doctrine:fixtures:load 
+@REM symfony console doctrine:fixtures:load 
